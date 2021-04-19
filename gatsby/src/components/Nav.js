0318e5f89ccb13063b80d95d4cee835f569358ae
mateustav <mateustav@gmail.com>
2021-04-18 23:48:20 -0700

    Add some slick styles to slick menu hehe

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -14,6 +14,35 @@ const NavStyles = styled.nav`
     grid-gap: 2rem;
     align-items: center;
   }
+
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
+  }
+  
 `;
 
 export default function Nav() {
